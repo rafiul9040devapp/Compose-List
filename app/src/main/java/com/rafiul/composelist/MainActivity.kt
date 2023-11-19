262fc505235d3b5cabf4dc012723cabd1dd355e3
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2023-11-19 09:39:39 +0600

    refactoring the whole code

--- app/src/main/java/com/rafiul/composelist/MainActivity.kt
+++ app/src/main/java/com/rafiul/composelist/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,100 +27,66 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeListTheme {
-                // A surface container using the 'background' color from the theme
-                var name by remember {
-                    mutableStateOf("")
-                }
-                val names = remember {
-                    mutableStateOf(listOf<String>())
-                }
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(all = 16.dp)
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        OutlinedTextField(
-                            value = name,
-                            onValueChange = { text ->
-                                name = text
-                            },
-                            modifier = Modifier.weight(1f)
-                        )
-
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        Button(
-                            modifier = Modifier.padding(all = 4.dp),
-                            onClick = {
-                                if (name.isNotBlank()) {
-                                    names.value += name
-                                    name = ""
-                                }
-                            },
-                        ) {
-                            Text(text = "ADD")
-                        }
-                    }
-
-                    ComponentsOfList(nameList = names) { deleteName ->
-                        names.value = names.value.filterNot { it == deleteName }
-                    }
-
-//                    ComponentsOfList(nameList = names, onDelete = { deleteName ->
-//                        names.value = names.value.filterNot { it == deleteName }
-//                    })
-
-//                    NameList(
-//                        names = names,
-//                        onDelete = { deleteName ->
-//                            names = names.filterNot { it == deleteName }
-//                        },
-//                    )
-
-                }
-            }
+          MyApp {
+              NameList()
+          }
         }
     }
+}
+
+@Composable
+fun MyApp( content: @Composable () -> Unit){
+    ComposeListTheme {
+        content()
+    }
+}
+
+@Composable
+fun NameList(){
+    var name by remember {
+        mutableStateOf("")
+    }
+    val names = remember {
+        mutableStateOf(listOf<String>())
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                value = name,
+                onValueChange = { text ->
+                    name = text
+                },
+                modifier = Modifier.weight(1f)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(
+                modifier = Modifier.padding(all = 4.dp),
+                onClick = {
+                    if (name.isNotBlank()) {
+                        names.value += name
+                        name = ""
+                    }
+                },
+            ) {
+                Text(text = "ADD")
+            }
+        }
+
+        ComponentsOfList(nameList = names) { deleteName ->
+            names.value = names.value.filterNot { it == deleteName }
+        }
 
 
-//    @Composable
-//    fun NameList(names: List<String>, onDelete: (String) -> Unit, modifier: Modifier = Modifier) {
-//        LazyColumn(modifier) {
-//            items(names) { currentName ->
-//                Row(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                ) {
-//
-//                    Text(
-//                        text = currentName,
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .padding(all = 16.dp)
-//                            .weight(1f)
-//                    )
-//                    Spacer(modifier = Modifier.width(16.dp))
-//
-//                    IconButton(onClick = {
-//                        onDelete(currentName)
-//                    }) {
-//                        Icon(
-//                            imageVector = Icons.Outlined.Delete,
-//                            contentDescription = "",
-//                            tint = Color.Red
-//                        )
-//                    }
-//                }
-//                Divider()
-//            }
-//        }
-//    }
+    }
 }
 
 
